5c0b1f69c16b4efa591912a629373d88e9c31ba2
Paul Power <ppower66@gmail.com>
2024-08-24 08:24:30 +1000

    initial commit

---
+++ src/LocalWeatherObservations.py
@@ -0,0 +1,23 @@
+"""
+    Name        : LocalWeatherObservations.py
+    Author      : Paul Power
+    Created     : 24-Aug-2024
+
+    Description : This program returns the weather from a publicly available API for a given location.  
+                  All temps are converted from kelvin to celsius and fahrenheit
+ 
+"""
+
+"""
+Todos:
+    1. Take a list of suburbs to report weather obs for and output json to either file or console
+    2. Develop a UI to enter parameters and present responses  
+    3. Allow setting of a timer to automate how frequently and how many iterations to report on (e.g vmstat)
+    4. After getting above working make a cloud API for this and run from Azure/AWS    
+"""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
